543c6be7bf605af820f56cf31cfb6084b7d06568
ls3205 <ls3205@trevor.org>
2022-02-19 13:00:40 -0500

    sheeeeeeeesh

--- src/index.ts
+++ src/index.ts
@@ -14,19 +14,33 @@ client.on('guildMemberAdd', member => {
     console.log(`${member.user.tag} has joined ${member.guild.name}, id: ${member.id}`);
     if (coucilList.includes(member.id)) {
         var role = member.guild.roles.cache.get('671061511648706571');
-        if (!role) return;
-        member.roles.add(role).catch(console.error);
-    }
-    if (member.id === '322868437745664004') { // just for tino
-        var role = member.guild.roles.cache.get('776164671161892905');
-        var role2 = member.guild.roles.cache.get('754028599656579162');
+        var role2 = member.guild.roles.cache.get('802612050005196891')
         if (!role) return;
         if (!role2) return;
         member.roles.add(role).catch(console.error);
         member.roles.add(role2).catch(console.error);
     }
+    if (member.id === '322868437745664004') { // just for tino
+        var role = member.guild.roles.cache.get('776164671161892905');
+        var role2 = member.guild.roles.cache.get('754028599656579162');
+        var role3 = member.guild.roles.cache.get('802612050005196891');
+        if (!role) return;
+        if (!role2) return;
+        if (!role3) return;
+        member.roles.add(role).catch(console.error);
+        member.roles.add(role2).catch(console.error);
+        member.roles.add(role3).catch(console.error);
+    }
     if (member.id === '604782316513919046') { // just for nate
         var role = member.guild.roles.cache.get('776164671161892905');
+        var role2 = member.guild.roles.cache.get('802612050005196891')
+        if (!role) return;
+        if (!role2) return;
+        member.roles.add(role).catch(console.error);
+        member.roles.add(role2).catch(console.error);
+    }
+    if (member.id === '314811526521290753') { // just for ted
+        var role = member.guild.roles.cache.get('635819127012851739');
         if (!role) return;
         member.roles.add(role).catch(console.error);
     }
